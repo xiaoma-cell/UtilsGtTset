f26b1060dd1ae1e1899df4acbcbd24ef46c00be4
Bing-Ma-Accenture <bing.ma@accenture.com>
2020-01-05 19:28:35 +0800

    new -90

---
+++ app/src/main/java/com/example/utilsgttset/Main4Activity.java
@@ -0,0 +1,19 @@
+package com.example.utilsgttset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        vdsvsdv
+                svvsdva
+                vsvasv
+                        vsvava
+                        vsvasva
+    }
+}
