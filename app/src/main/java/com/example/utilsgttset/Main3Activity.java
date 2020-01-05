adc984a4d600b1ae8c1f496ea91c30b8f61bf47b
Bing-Ma-Accenture <bing.ma@accenture.com>
2020-01-05 19:26:17 +0800

    new -033

---
+++ app/src/main/java/com/example/utilsgttset/Main3Activity.java
@@ -0,0 +1,29 @@
+package com.example.utilsgttset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
+FVASV
+
+CSV
+        ASV
+V
+                SAV
+AS
+                        ASV
+SSSS
+                                String SS
+                                        SSS
+S
+                                                String S
+                                                        String S
+                                                                S
